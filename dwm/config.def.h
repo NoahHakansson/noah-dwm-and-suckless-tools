2f6053ecbf24772202101b7e09bbcfdfb060ad27
Noah <noah@pop-os.localdomain>
2021-03-29 15:07:43 +0200

    small rule for mail clients to be floating.

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -44,7 +44,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class              instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",              NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Gimp",              NULL,     NULL,           0,         1,          0,           1,        -1 },
+	{ "Mail",       NULL,     NULL,           0,         1,          0,           1,        -1 },
 	{ "Pavucontrol",       NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "discord",           NULL,     NULL,           1 << 7,    0,          0,          -1,         0 },
 	{ "Tilix",             NULL,     NULL,           0,         0,          1,           0,        -1 },

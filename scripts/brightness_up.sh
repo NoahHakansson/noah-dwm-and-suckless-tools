e0d86aa6954b4e92b5ae0034abe12a612489641f
Noah <nallephu12@gmail.com>
2021-04-08 09:12:57 +0200

    changed slstatus to show volume and be refreshes on vol_change + keybind changes

--- scripts/brightness_up.sh
+++ scripts/brightness_up.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+    # change brightness
     sudo brightnessctl set +10%
+    # notify
     dunstify -a "changeVolume" -u low -i /home/noah/.config/dunst/brightness.png -r 1212 "Brightness" "$(brightnessctl -m | awk -F[=,] '{print $4}')"
-
+    # Play the volume changed sound
+    canberra-gtk-play -i audio-volume-change -d "changeVolume"
--- scripts/vol_up.sh
+++ scripts/vol_up.sh
@@ -6,13 +6,15 @@ then
     # check if muted
     if [ $(pamixer --get-mute) = "true" ]
     then
-        #notify
+        #notify muted
         dunstify -a "changeVolume" -u low -i /home/noah/.config/dunst/volume-mute.png -r 1212 "Volume" "$(pamixer --get-volume-human)"
     else
         # change volume
         pactl set-sink-volume @DEFAULT_SINK@ +5%
+        # reload slstatus
+        killall slstatus -HUP; slstatus&
         # notify
-        dunstify -a "changeVolume" -u low -i /home/noah/.config/dunst/volume.png -r 1212 "Volume" "$(pamixer --get-volume-human)"
+        #dunstify -a "changeVolume" -u low -i /home/noah/.config/dunst/volume.png -r 1212 "Volume" "$(pamixer --get-volume-human)"
         # Play the volume changed sound
         canberra-gtk-play -i audio-volume-change -d "changeVolume"
     fi

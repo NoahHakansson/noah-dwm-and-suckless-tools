a7a1ba452402a63ca9712430a29e54e13dc67c89
Noah <nallephu12@gmail.com>
2022-03-06 12:37:26 +0100

    Changed to bottom bar.

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -11,7 +11,7 @@ static const unsigned int systrayspacing = 0;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar, 1 means top bar*/
 static const char *fonts[]          = { "monospace:size=11", "JetBrainsMono Nerd Font Regular:size=11" };
 static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#1d2021"; // background color
